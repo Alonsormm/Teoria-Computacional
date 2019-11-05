71add87fd209051f5aa6f1e0d549e52881226061
Alonso Rubén Maya Martínez <alonsormm@pop-os.localdomain>
2019-11-04 20:57:30 -0600

    Practica 7 terminada

--- Practica 7/regularexpresion.py
+++ Practica 7/regularexpresion.py
@@ -1,5 +1,8 @@
 import random
 
+
+###Evaluador de expresion por arbol
+
 class Nodo(object):
     def __init__(self,dato):
         self.dato = dato
@@ -7,63 +10,8 @@ class Nodo(object):
         self.der = None
     def imprimirNodo(self):
         print(self.dato)
-
-class BinaryTree(object):
-    def __init__(self, dato = None):
-        self.raiz = Nodo(dato)
-
-class RegEx(object):
-    def __init__(self, regex):
-        self.regex = regex
-        self.regexOriginal = regex
-        self.cadenas = []
-        self.arbol = BinaryTree()
-        self.completarRegEx()
-    def isCharacter(self,car):
-        if car == "1" or car == "0" or car == "c":
-            return True
-        else:
-            return False
-    def completarRegEx(self):
-        i = 0
-        tam = len(self.regex) - 1
-        while(i < tam):
-            if self.isCharacter(self.regex[i]) and self.isCharacter(self.regex[i +1]):  
-                self.regex= self.regex[:i+1] + 'x' + self.regex[i+1:]
-                tam +=1
-            if self.regex[i+1] == '*':
-                self.regex= self.regex[:i+2] + 'c' + self.regex[i+2:]
-                tam +=1
-            i+=1
-    def generarCadenas(self,raiz):
-        if isCharacter(raiz.dato):
-            return raiz.dato
-        if raiz.dato == '+':
-            return self.suma(self.generarCadenas(raiz.izq),self.generarCadenas(raiz.der))
-        if raiz.dato == 'x':
-            return self.producto(self.generarCadenas(raiz.izq), self.generarCadenas(raiz.der))
-        if raiz.dato == '*':
-            return self.universo(self.generarCadenas(raiz.izq))
-    ###Operaciones
-    def suma(self, a,b):
-        rand = random.randint(0,2)
-        if rand == 0:
-            return a
-        if rand == 1:
-            return b
-        if rand == 2:
-            return a+b
-    def producto(self, a, b):
-        return a+b
-    def universo(self,a):
-        final = ""
-        for i in range(random.randint(0,100)):
-            final += a
-        return final
-    
-###Evaluador de expresion por arbol
 def isCharacter(car):
-    if car == "1" or car == "0" or car == "c":
+    if car == "1" or car == "0" or car == "c"  or car == "e":
         return True
     else:
         return False
@@ -74,8 +22,8 @@ def precedence(car):
     if car == '*':
         return 2
     if car == 'x':
-        return 0
-    return 1
+        return 1
+    return 0
 def existOperant(expresion):
     for i in expresion:
         if isOperant(i):
@@ -87,6 +35,12 @@ def inorder(raiz):
     inorder(raiz.izq)
     raiz.imprimirNodo()
     inorder(raiz.der)
+def postorder(raiz):
+    if raiz == None:
+        return
+    inorder(raiz.izq)
+    inorder(raiz.der)
+    raiz.imprimirNodo()
 def expressionToTree(raiz, t):
     if not existOperant(t):
         return Nodo(t)
@@ -120,10 +74,73 @@ def expressionToTree(raiz, t):
     raiz.der = expressionToTree(raiz.der,derecha)
     return raiz
 
+class RegEx(object):
+    def __init__(self, regex):
+        self.regex = regex
+        self.regexOriginal = regex
+        self.cadenas = []
+        self.completarRegEx()
+    def isCharacter(self,car):
+        if car == "1" or car == "0" or car == "c" or car == "e":
+            return True
+        else:
+            return False
+    def completarRegEx(self):
+        i = 0
+        tam = len(self.regex) - 1
+        while(i < tam):
+            if self.isCharacter(self.regex[i]) and self.isCharacter(self.regex[i +1]) or self.isCharacter(self.regex[i]) and self.regex[i+1] == '(':  
+                self.regex= self.regex[:i+1] + 'x' + self.regex[i+1:]
+                tam +=1
+            if self.regex[i+1] == '*':
+                self.regex= self.regex[:i+2] + 'c' + self.regex[i+2:]
+                tam +=1
+            i+=1
+    def generarCadena(self,raiz, universo = False):
+        if isCharacter(raiz.dato):
+            return raiz.dato
+        if raiz.dato == '+':
+            if universo:
+                return self.suma(self.universo(self.generarCadena(raiz.izq,True)),self.universo(self.generarCadena(raiz.der,True)))
+            return self.suma(self.generarCadena(raiz.izq),self.generarCadena(raiz.der))
+        if raiz.dato == 'x':
+            if universo:
+                return self.producto(self.generarCadena(raiz.izq,True), self.generarCadena(raiz.der, True))
+            return self.producto(self.generarCadena(raiz.izq), self.generarCadena(raiz.der))
+        if raiz.dato == '*':
+            return self.generarCadena(raiz.izq,True)
+    ###Operaciones
+    def suma(self, a,b):
+        rand = random.randint(0,2)
+        if rand == 0:
+            if a == 'e':
+                return ""
+            return a
+        if rand == 1:
+            if b == 'e':
+                return ""
+            return b
+        if rand == 2:
+            if a == 'e':
+                return b
+            if b == 'e':
+                return a
+            return a+b
+    def producto(self, a, b):
+        return a+b
+    def universo(self,a):
+        final = ""
+        if a == 'e':
+            return ""
+        for i in range(random.randint(0,100)):
+            final += a
+        return final
 
-regexaa = input()
-reg = RegEx(regexaa)
+
+
+ExpresionRegular = input()
+reg = RegEx(ExpresionRegular)
 arbol = Nodo(None)
 arbol = expressionToTree(arbol, reg.regex)
-#inorder(arbol)
-print(reg.generarCadenas(arbol))
+for i in range(100):
+    print(reg.generarCadena(arbol))
