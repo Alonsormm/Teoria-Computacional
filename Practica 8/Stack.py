5cb47c17e3e3d96777a48bdc684d145cec3afe72
Alonso Rubén Maya Martínez <alonsormm@pop-os.localdomain>
2019-11-27 20:47:17 -0600

    Tercera entrega terminada

--- Practica 8/Stack.py
+++ Practica 8/Stack.py
@@ -12,28 +12,26 @@ s=Stack()
 
 window = Tk()
 window.geometry('600x600')
-window.configure(bg = 'beige')
 window.title("Generador y analizador de cadenas de la forma L={0^n1^n|n>=1,neR}")
 window.resizable(width=False,height=False)
-canvas = Canvas(window) 
-canvas.create_oval(500, 500, 80, 80,outline = "black", fill = "white",width = 2)
-Texto=Text(window, width=61, height=10)
+canv = Canvas(window,width=600,height=600)
+
+Texto=Text(window, width=49, height=10)
 Texto.place(x=10,y=10)
 etiqueta = Label(window, text="Aqui se mostrara si se acepto la cadena")
-etiqueta.place(x=100,y=180)
+etiqueta.place(x=10,y=180)
+etiqueta2 =Label(window, text="Aqui se mostrara como se va evaluando la cadena")
+etiqueta2.place(x=10,y=210)
+
+
+
+
+canv.pack() 
+xspeed=5
+yspeed=0
 
 Texto.place(x=100,y=10)
-arreglo = []
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1,bg="blue"))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
-arreglo.append(Button(window, text="0", font="Helvetica 16 bold", height=1, width=1))
+
 def generar():
     Texto.delete("1.0",END)
     cadena = ""
@@ -49,42 +47,88 @@ def evaluar():
     print(cadena)
     s.push("z")
     estado_actual="q0"
-    contador=0
+    y1=540
+    y2=545
+    aux=0
+    
+    cadena_imprimir="La cadena se esta evaluando asi: "
+    if len(cadena)>60:
+        etiqueta2.config(text="Tu cadena es muy grande, no se mostrara animacion, unicamente se te dira si se acepta o no")
+    else:
+        canv.create_rectangle(300, 200, 400, 550, width=0, fill='black')
+        #for a in range(0,60):
+            #canv.create_rectangle(305, 540, 395, 545, width=0, fill='red')
+            #canv.create_rectangle(305, 535, 395, 539, width=0, fill='red')
+            #canv.create_rectangle(305, 529, 395, 534, width=0, fill='red')
+            #canv.create_rectangle(305, y1, 395, y2, width=0, fill='red')
+            #y1=529
+            #y2=534
+            #canv.create_rectangle(305, 529, 395, 534, width=0, fill='red')
+            #canv.create_rectangle(305, 535, 395, 539, width=0, fill='red')
+            #canv.create_rectangle(305, 540, 395, 545, width=0, fill='red')
+#
+            #canv.create_rectangle(305, y1, 395, y2, width=0, fill='red')
+#
+            #canv.create_rectangle(305, y2+1, 395, y2+10, width=0, fill='red')
+            #aux=y1
+            #y1=y2+1
+            #y2=aux+10
+
     for caracter_actual in cadena:
-        contador+=1
+        cadena_imprimir+=caracter_actual
+
+        window.update()
+        if len(cadena)<=60:
+            etiqueta2.config(text=cadena_imprimir)
+        time.sleep(.5)
         if estado_actual=="q0" and caracter_actual=="0":
-            arreglo[contador].after(3000, arreglo[contador].place(x=300,y=(500-(contador*45)))) 
-            #arreglo[contador].place(x=300,y=(500-(contador*45)))
+            if len(cadena)<60:
+                canv.create_rectangle(305, y1, 395, y2, width=0, fill='red')
 
             s.push("a")
             estado_actual="q0"
+            aux=y1
+            y1=y2-10
+            y2=aux-1
             continue
         if not s.isEmpty():
             popeado = s.pop()
-            arreglo[contador].after(3000, arreglo[contador].place_forget) 
+
         if estado_actual=="q0" and caracter_actual=="1" and popeado=="a":
+            aux=y1
+            y1=y2+1
+            y2=aux+10
+            if len(cadena)<60:
+                canv.create_rectangle(305, y1, 395, y2, width=0, fill='black')
             estado_actual="q1"
         elif estado_actual=="q1" and caracter_actual=="1" and popeado=="a":
+            if len(cadena)<60:
+                canv.create_rectangle(305, y1, 395, y2, width=0, fill='black')
             estado_actual="q1"
         else:
-            etiqueta.config(text="Asi nos toco vivir")
+            etiqueta.config(text="No se acepta la cadena")
             return
+        aux=y1
+        y1=y2+1
+        y2=aux+10
+    window.update()
     if not s.isEmpty():
         popeado = s.pop()
+        if len(cadena)<60:
+            canv.create_rectangle(305, 540, 395, 545, width=0, fill='black')
     if estado_actual=="q1" and popeado=="z":
         estado_actual="q2"
     else:
-        etiqueta.config(text="Asi nos toco vivir")
+        etiqueta.config(text="No se acepta la cadena")
         return
     if estado_actual=="q2":
-        etiqueta.config(text="Viva Mexico")
+        etiqueta.config(text="Se acepta la cadena")
     else:
         return
 
 
 
 
-#Texto.insert("1.0", cadena)
 opcion1=ttk.Button(window, text='Generar Cadena', command=generar)
 opcion1.place(x=0,y=10)
 opcion2=ttk.Button(window, text='Evaluar Cadena', command=evaluar)
